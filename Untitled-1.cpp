143dbc0b0808a6e155f61ef7872934c9185a9e8a
samipatel12345678 <155371078+samipatel12345678@users.noreply.github.com>
2024-01-18 12:51:30 +0530

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+  srand((unsigned int )(NULL));
+  int number=(rand() % 100)+1;
+  int guess =0;
+  do{
+    cout<<"Enter Guess  (0-100):";
+  cin>>guess;
+  if(guess > number){
+    cout<<"GUESS LOWER!"<<endl;
+  }
+    else if( guess < number){
+cout<<"GUESS HIGHER!"<<endl;
+
+    }
+    else
+    cout<<"YOU WON "<<endl;
+
+  }
+   while (guess!=number);
+ return 0;
+}
